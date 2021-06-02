815c47fd7bad3de55b4d6c088c048fdd8e8d83b6
bfwissel <50451295+bfwissel@users.noreply.github.com>
2021-06-02 12:32:11 -0400

    Added "uploading" message when waiting for the image to upload

--- FoodForUsAll/Services/ImageAdder.cs
+++ FoodForUsAll/Services/ImageAdder.cs
@@ -9,16 +9,20 @@ namespace Services
 	// ??? Intended for the ultimate migration to saving images to the file system instead of the database.
     public class ImageAdder : IImageAdder
 	{
-		public ImageAdder()
+		public ImageAdder(string connectionString)
 		{
+			_foodForUsAllConnectionString = connectionString;
 		}
 
 		public async Task Add()
 		{
-
+			if (_foodForUsAllConnectionString == null)
+				throw new ArgumentException("Unable to locate the FoodForUsAllConnectionString withing the configuration file.");
 		}
 
 		#region private
+
+		readonly string _foodForUsAllConnectionString;
 
 		#endregion
 	}
